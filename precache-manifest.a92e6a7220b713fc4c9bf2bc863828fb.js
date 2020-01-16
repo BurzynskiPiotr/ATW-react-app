1db66bd4ef9284a31661e2f2bb4631117c34055c
Piotr <51122302+BurzynskiPiotr@users.noreply.github.com>
2020-01-16 18:24:54 +0100

    Updates

---
+++ precache-manifest.a92e6a7220b713fc4c9bf2bc863828fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5c9c7394c20bacac57426262e159e4c",
+    "url": "/ATW-react-app/index.html"
+  },
+  {
+    "revision": "9bf2e507f318b90c5879",
+    "url": "/ATW-react-app/static/css/main.755ba410.chunk.css"
+  },
+  {
+    "revision": "d2af3c3ad4636123457a",
+    "url": "/ATW-react-app/static/js/2.45fe111d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ATW-react-app/static/js/2.45fe111d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9bf2e507f318b90c5879",
+    "url": "/ATW-react-app/static/js/main.29d1b168.chunk.js"
+  },
+  {
+    "revision": "0caedff34937251a0be1",
+    "url": "/ATW-react-app/static/js/runtime-main.6b907872.js"
+  }
+]);
